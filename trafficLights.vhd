a92e83cfbf00f7e105de59a0adab9d539a9177b6
Franciszek Moszczuk <90723171+avgsurfman@users.noreply.github.com>
2025-01-28 11:15:18 +0100

    Update trafficLights.vhd

--- trafficLights.vhd
+++ trafficLights.vhd
@@ -19,7 +19,7 @@ component Opis_D_FF
 );
 end component;
 
---deklaracje sygna³ów
+--deklaracje sygnaÂ³Ã³w
 signal stan: bit_vector(1 downto 0); --stan: new state	
 signal Q : bit_vector(1 downto 0);
 signal nQ : bit_vector(1 downto 0); --nQ -> present negated state
@@ -38,6 +38,7 @@ begin
 		else
 			stan(1) <= (Q(0) and W) or (Q(0) and Q(1)) or (Q(1) and not(W));
 			stan(0) <= (Q(1) and W) or (nQ(0) and Q(1)) or (Q(1) and not(W));
+			assert ((Q(0) and W)='1') report "Triggered" severity note;
 		end if;
 	end process;
 	S1 : entity D_FF port map(stan(1), Clk, Q(1), nQ(1), R, R_sync);
@@ -51,8 +52,10 @@ begin
 	-- Stan_0 <= (nQ(1) nor not(W)) or (nQ and not(nQ(1))) or (nQ(1) nor W)
 	--stan(1) <= (nQ(0) nor not(W)) or  (nQ(0) nor nQ(1)) or  (nQ(1) nor W); --> Simplified and optimized expr
 	--stan(0) <= (nQ(1) nor not(W)) or (nQ(0) and Q(1)) or (nQ(1) nor W);
+	
 
 	-- https://stackoverflow.com/questions/19412165/how-to-ignore-output-ports-with-port-maps
+	-- ((0) nor Q(1))
 	Czerwony <= (stan(0) nor stan(1)) or (not(stan(1)) and stan(0) and not(W)) or (stan(1) and not(stan(0)) and W);
 	Zolty <= ((stan(0) nor stan(1)) and W) or (not(stan(0)) and stan(1) and not(W)) or (stan(0) and stan(1) and W) or (stan(0) and not(stan(0)) and not(W));
 	Zielony <= (not(stan(0)) and stan(1) and W) or (stan(0) and stan(1) and not(W));
